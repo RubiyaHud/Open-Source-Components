1a2f244c12f6035790915879bbf019e53b1aad7a
RubiyaHud <rubiya.hud@gmail.com>
2025-05-10 15:46:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import UiverseBtnTailCSS from './components/UiverseBtnTailCSS'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-amber-200 mb-6 text-center font-bold text-2xl'>Open-Source buttons made with TailwindCSS (https://uiverse.io/)(https://uiverse.io/Rauliqbal/average-ladybug-82) </h1>
+      <div className="m-auto text-center">
+        <UiverseBtnTailCSS />
+      </div>
+      
+    </>
+  )
+}
+
+export default App

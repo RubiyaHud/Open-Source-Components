02c01efc80492d87972199df0d6f55f881f2a547
RubiyaHud <rubiya.hud@gmail.com>
2025-05-11 11:57:33 +0600

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 
 import './App.css'
+import Footer from './components/Footer'
 import UiverseBtnTailCSS from './components/UiverseBtnTailCSS'
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-amber-200 mb-6 text-center font-bold text-2xl'>Open-Source buttons made with TailwindCSS (https://uiverse.io/)(https://uiverse.io/Rauliqbal/average-ladybug-82) </h1>
+      <h1 className='bg-amber-200 mb-6 text-center font-bold text-2xl'>Open-Source button made with TailwindCSS (https://uiverse.io/)(https://uiverse.io/Rauliqbal/average-ladybug-82) </h1>
       <div className="m-auto text-center">
         <UiverseBtnTailCSS />
       </div>
-      
+
+      <h1 className='bg-amber-200 mb-6 text-center font-bold text-2xl mt-9'>
+        Open-Source Footer made with TailwindCSS (https://daisyui.com/)(https://daisyui.com/components/footer/) </h1>
+      <div className='max-w-[1170px] m-auto'>
+        <Footer />
+      </div>
+
     </>
   )
 }
